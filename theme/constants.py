84c0ebf2f31bb79ad830a2c3482ffeec51272985
Ken Guan <ruihan.guan@you.world>
2019-08-26 23:02:11 -0700

    rewrite Themes to be customizable

---
+++ theme/constants.py
@@ -0,0 +1,21 @@
+
+theme = 'Theme'
+
+quiet = 'Quiet Theme'
+engaged = 'Engaged Theme'
+romantic = 'Romantic Theme'
+cozy = 'Cozy'
+reading = 'Concentrated'
+sleep = 'Sleep'
+
+image = 'Image'
+light = 'Light'
+music = 'Music'
+seat_angle = 'Seat Angle'
+reading_light = 'Reading Light'
+entertainment_recommendation = 'Entertainment Recommendation'
+
+turbulance = 'Turbulance'
+ambiant_light = 'Ambiant Light'
+safety_belt_warning = 'Safety Belt Warning'
+entertainment_pause = 'Entertainment Pause'
