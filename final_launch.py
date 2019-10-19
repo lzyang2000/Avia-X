83e4dd6ee0cc770de73044fb0d2b6fc5ae6c986e
Alan He <xhe22@berkeley.edu>
2019-10-19 15:37:25 -0700

    Testing music

--- final_launch.py
+++ final_launch.py
@@ -35,7 +35,7 @@ class GUISession(Session):
     def __init__(self):
 
         # pygame.mixer.init()
-        pygame.mixer.pre_init(frequency=44100, size=-16, channels=2, buffersize=4096)
+        pygame.mixer.pre_init(frequency=44100, size=-16, channels=2)
         self.curr_music = None
         app = App(title="Avia-X is running")
         self.app = app
