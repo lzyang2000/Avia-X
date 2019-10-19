4b29cceab09407956b2a163ee708a1b96b37b093
Alan He <xhe22@berkeley.edu>
2019-10-19 16:44:07 -0700

    Set music volume success - set vol int

--- final_launch.py
+++ final_launch.py
@@ -98,9 +98,9 @@ class GUISession(Session):
 
     def set_music_volume(self, vol):
         # pygame.mixer.music.set_volume(int(vol) * 1.00 / 100)
-        self.music_players[quiet].audio_set_volume(vol)
-        self.music_players[engaged].audio_set_volume(vol)
-        self.music_players[warm].audio_set_volume(vol)
+        self.music_players[quiet].audio_set_volume(int(vol))
+        self.music_players[engaged].audio_set_volume(int(vol))
+        self.music_players[warm].audio_set_volume(int(vol))
         
     def play_music(self, prev_theme, new_theme):
         if prev_theme == new_theme:
