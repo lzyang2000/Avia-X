08703fca663ec4372db3a83c49e15e04c0bd6acf
Gary Yang <lzyang@berkeley.edu>
2019-10-19 15:24:41 -0700

    Update image_emotion_demo.py

--- emotion_recognition/src/image_emotion_demo.py
+++ emotion_recognition/src/image_emotion_demo.py
@@ -35,7 +35,7 @@ emotion_target_size = emotion_classifier.input_shape[1:3]
 camera = picamera.PiCamera()
 camera.resolution = (1024, 768)
 camera.rotation = 180
-camera.start_preview()# -w 1296 -h 972 #-p ('50,50,950,950')
+camera.start_preview(fullscreen=False,window=(100,200,300,400))# -w 1296 -h 972 #-p ('50,50,950,950')
 #camera.preview.window = (50,50,90,90)
 #camera.start_preview()
 # camera = cv2.VideoCapture(0)
