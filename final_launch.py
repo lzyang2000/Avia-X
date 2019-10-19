90bf4afa54555ba81c11d19bbda2d1de1008807b
Alan He <xhe22@berkeley.edu>
2019-10-19 16:06:28 -0700

    Changing music freq

--- final_launch.py
+++ final_launch.py
@@ -34,7 +34,7 @@ class GUISession(Session):
 
     def __init__(self):
 
-        pygame.mixer.pre_init(frequency=44100)
+        pygame.mixer.pre_init(frequency=48000)
         pygame.mixer.init()
         self.curr_music = None
         app = App(title="Avia-X is running")
