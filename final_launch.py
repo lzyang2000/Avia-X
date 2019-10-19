c87970a80578ff5b9738155ae65c33c9ddc9aa68
Alan He <xhe22@berkeley.edu>
2019-10-18 21:44:49 -0700

    Fixed everything about prev_turb - hopefully...

--- final_launch.py
+++ final_launch.py
@@ -28,7 +28,7 @@ class Session:
 
 class GUISession(Session):
 
-    state = { turbulence: 1 , luminance : 3, "prev_turbulences": None, theme: None }
+    state = { turbulence: 1 , luminance : 3, 'prev_turbulences': None, theme: None }
     idx = 0
 
     def __init__(self):
@@ -117,16 +117,14 @@ class GUISession(Session):
 
     def update_state(self):
         self.idx += 1
-        # if self.state[prev_turbulences]:
-        if hasattr(self, prev_turbulences):
-            self.all_infos = Infos(self.idx, self.state[prev_turbulences])
+        if hasattr(self, 'prev_turbulences'):
+            self.all_infos = Infos(self.idx, self.prev_turbulences)
         else:
             self.all_infos = Infos(self.idx)
         self.state[turbulence] = self.all_infos.turbulence
         self.state[luminance] = self.all_infos.light
         self.state[emotion] = self.all_infos.emotion
         self.state[pressure] = self.all_infos.pressure
-        # self.state[prev_turbulences] = self.all_infos.prev_turbulences
         self.prev_turbulences = self.all_infos.prev_turbulences
 
         # Light up the Rasp Pi
@@ -185,7 +183,6 @@ class Infos:
 
     def lightPi(self, theme):
         ledout.change_color(theme)
-        # pass
 
 def main():
     session = GUISession()
